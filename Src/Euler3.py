85b1923cd64657ebca6cc81542a799bf0fbfc23a
unknown <joewledger@gmail.com>
2015-08-08 15:38:46 -0400

    Updated Euler3.py to use Prime_Generator.py
    Signed-off-by: unknown <joewledger@gmail.com>

--- Src/Euler3.py
+++ Src/Euler3.py
@@ -2,27 +2,11 @@
 #Finds the largest prime factor of 600851475143
 #Uses sieve of eratosthenes
 from time import time
-t1 = time()
+#t1 = time()
+import Prime_Generator as p_gen
+import time
 
-
-maxCheck = 100000
-primes = [False, False, True, True, False, True]
-for index in range(6,maxCheck):
-	primes.append(True)
-for index in range(2, maxCheck):
-	if(primes[index] == True):
-		i2 = index
-		i2 += index
-		while(i2 < maxCheck):
-			primes[i2] = False
-			i2 += index
-a = maxCheck - 1
-satisfy = False
-while(a > 0):
-	if(primes[a] == True):
-		if(600851475143%a == 0):
-			print(a)
-	a -= 1
-
-t2 = time()
-print((t2 - t1))
+time.clock()
+primes = p_gen.generate_primes(10000,sieve="eratosthenes")
+print("Answer: %s" % str(max(prime for prime in primes if 600851475143 % prime == 0)))
+print("Calculation took %s seconds" % str(time.clock()))
