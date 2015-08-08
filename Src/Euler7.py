d5e8fa1b78f0888e359771d85df38f39ad59be6b
unknown <joewledger@gmail.com>
2015-08-08 17:03:28 -0400

    Implented Euler 7, but solution is not fast. Should write more efficient code to generate nth prime.
    Signed-off-by: unknown <joewledger@gmail.com>

--- Src/Euler7.py
+++ Src/Euler7.py
@@ -1,18 +1,12 @@
 #Project Euler Problem 7
 #Finds 10001st prime
 
-maxCheck = 200000
-primes = [False, False, True, True, False, True]
-for index in range(6,maxCheck):
-	primes.append(True)
-for index in range(2, maxCheck):
-	if(primes[index] == True):
-		i2 = index
-		i2 += index
-		while(i2 < maxCheck):
-			primes[i2] = False
-			i2 += index
-primeCount = 0
-for index in range(0, len(primes)):
-	if(primes[index]): primeCount += 1
-	if(primeCount == 10001 and primes[index]): print(index)
+import Prime_Generator as p_gen
+import time
+time.clock()
+
+max_n = 200000
+value = p_gen.generate_primes(max_n,"eratosthenes")[10000]
+
+print("Answer: %s" % value)
+print("Calculation took %s seconds" % str(time.clock()))
