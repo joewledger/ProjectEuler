b5d721ca22780c1d6da7926dff8fa8f21761f918
unknown <joewledger@gmail.com>
2015-08-08 15:59:20 -0400

    Updated Euler5 to only check numbers that are the product of the primes in the range (1,20). Improved efficiency by an order of magnitude.
    Signed-off-by: unknown <joewledger@gmail.com>

--- Src/Euler5.py
+++ Src/Euler5.py
@@ -1,16 +1,15 @@
 #Project Euler Problem 5
 #Finds the smallest number evenly divisible by each of the numbers 1-20
 
-satisfied = False
-a = 20
-while(not satisfied):
-	a += 20
-	check = True
-	cN = 1
-	while(cN < 21 and check == True):
-		if(not a%cN == 0):
-			check = False
-		cN += 1
-	if(check == True):
-		satisfied = True
-		print(a)
+import time
+from operator import mul
+import Prime_Generator as p_gen
+time.clock()
+
+primes = p_gen.generate_primes(20,sieve="eratosthenes")
+prod_primes = reduce(mul,primes,1)
+value = prod_primes
+while(not all(value % x == 0 for x in xrange(1,21))):
+	value += prod_primes
+print("Answer: %s" % value)
+print("Calculation took %s seconds" % str(time.clock()))
