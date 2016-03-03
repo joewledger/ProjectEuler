58deeb4f5080d7c1f4726b06b1939c4d23141b4f
joewledger <joewledger@gmail.com>
2016-03-03 15:34:13 -0500

    Finished check_answers script.
    Signed-off-by: joewledger <joewledger@gmail.com>

--- check_answers.py
+++ check_answers.py
@@ -11,37 +11,53 @@ def main():
     answer_dict = get_answer_dictionary()
 
     problem_dirs = [x[0] for x in os.walk("Problems")][1:]
-    for path in problem_dirs:
+    for path in sorted(problem_dirs, key = lambda x: parse_problem_number(x)):
+        prob_num = parse_problem_number(path)
         if "run.sh" in os.listdir(path):
-            p = multiprocessing.Process(target=run_problem, name="",args=(path,answer_dict,))
+            
+
+            p = multiprocessing.Process(target=run_problem, name="",args=(path,prob_num,answer_dict,))
             p.start()
 
             p.join(max_seconds)
 
             time.sleep(max_seconds)
             if p.is_alive():
-                print("Terminating: %s" % path)
-                p.terminate()
-                p.join()
-                proc = subprocess.Popen("ps aux | pgrep -f %s" % path, stdout=subprocess.PIPE, shell=True)
-                (out, err) = proc.communicate()
-                for s in out.split("\n")[:-1]:
-                    DEVNULL = open(os.devnull, 'wb')
-                    subprocess.Popen("kill -9 %d" % int(s), stdout=DEVNULL, stderr=DEVNULL,shell=True).communicate()
-                    DEVNULL.close()
+                terminate_euler_process(path,prob_num, p)
+        else:
+            pretty_print_status(prob_num, "No run.sh script found")
 
-def run_problem(path,answer_dict):
+def terminate_euler_process(path,prob_num,process):
+    pretty_print_status(prob_num,"Terminated early")
+    process.terminate()
+    process.join()
+    pkill = subprocess.Popen("ps aux | pgrep -f %s" % path, stdout=subprocess.PIPE, shell=True)
+    (out, err) = pkill.communicate()
+    for s in out.split("\n")[:-1]:
+        DEVNULL = open(os.devnull, 'wb')
+        subprocess.Popen("kill -9 %d" % int(s), stdout=DEVNULL, stderr=DEVNULL,shell=True).communicate()
+        DEVNULL.close()
+
+
+def run_problem(path,prob_num,answer_dict):
     run_file = "%s/%s" % (path,"run.sh")
-    problem_number = int(re.search(r'\d+$', path).group(0))
-    correct_answer = answer_dict[problem_number]
+    try:
+        correct_answer = answer_dict[prob_num]
+    except KeyError:
+        pretty_print_status(prob_num, "Correct Answer not Specified")
+        return
 
     os.system("chmod +x %s" % run_file)
-    proc = subprocess.Popen("time -f %%E %s %s" % (run_file,path), stdout=subprocess.PIPE, shell=True)
+    DEVNULL = open(os.devnull, 'wb')
+    proc = subprocess.Popen("time -f %%E %s %s" % (run_file,path), stdout=subprocess.PIPE,stderr=DEVNULL, shell=True)
+    DEVNULL.close()
     (out, err) = proc.communicate()
-    if not err:
-        print("Problem: %d\tCorrect Answer: %d\tProgram Answer: %s" % (problem_number, correct_answer,out.strip()))
-    else:
-        print("Problem: %d\t Error: %s" % (problem_number,err))
+    try:
+        out = int(out.strip())
+        correct = "Correct" if (correct_answer == out) else "Incorrect"
+        pretty_print_status(prob_num, "%s (%d,%d)" % (correct, correct_answer, out))
+    except ValueError:
+        pretty_print_status(prob_num,"Did not provide numeric answer")
 
 def get_answer_dictionary():
     answers = {}
@@ -51,6 +67,12 @@ def get_answer_dictionary():
             l = line.split(":")
             answers[int(l[0])] = int(l[1])
     return answers
+
+def parse_problem_number(path):
+    return int(re.search(r'\d+$', path).group(0))
+
+def pretty_print_status(prob_num, message):
+    print("Problem: %d\t%s" % (prob_num,message))
 
 if __name__ == "__main__":
     main()  
